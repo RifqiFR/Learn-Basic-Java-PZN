35a08e7ed0ffedfa7106ab7a70dc3ea5230c7c08
Rifqi Fajar Ramdani <rifqifajar24@gmail.com>
2022-11-09 16:19:46 +0700

    Palindrome , Devide dan Conquer

---
+++ src/Latihan/Palindrome.java
@@ -0,0 +1,25 @@
+package Latihan;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        //Write your code here
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine().toLowerCase().replaceAll(" ", "");
+        podowae(input);
+    }
+
+    static void podowae(String original){
+        String reverse ="";
+        int length = original.length();
+
+        for (int i = length - 1; i >= 0; i--)
+            reverse = reverse + original.charAt(i);
+
+        if (original.equals(reverse))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
